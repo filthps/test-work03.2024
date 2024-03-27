7b95347d9d277a07bab7f344120531a716dc5241
Victor-PC <filpsixi@mail.ru>
2024-03-27 08:00:52 +0300

    db

--- test_app/api/models.py
+++ test_app/api/models.py
@@ -1,5 +1,34 @@
+import re
+import uuid
 from django.db import models
+from django.core.exceptions import ValidationError
+from django.contrib.auth.models import User
+
+
+MAX_WORDS_COUNT = 200
+
+
+def check_multi_words(s: str):
+    words_count = len(re.match(r"\b", s).groups())
+    if words_count > 1:
+        raise ValidationError
+    if words_count > MAX_WORDS_COUNT:
+        raise ValidationError
 
 
 class Text(models.Model):
-    text = models.TextField(blank=False)
+    id = ...
+    add_by = models.ForeignKey(User, on_delete=models.PROTECT)
+    date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.id}[{self.add_by.username}] {self.date.strftime('%Y-%m-%d %H:%M')}"
+
+
+class Word(models.Model):
+    id = models.CharField(default=lambda: uuid.uuid4().__str__(), primary_key=True, max_length=100)
+    text = models.ForeignKey(Text, on_delete=models.CASCADE, blank=False)
+    word = models.CharField(max_length=40, blank=False, validators=[check_multi_words])
+
+    def __str__(self):
+        return str(self.word)
--- test_app/api/serializers.py
+++ test_app/api/serializers.py
@@ -1,11 +1,21 @@
+import re
 from rest_framework import serializers
-from models import Text
+from .models import MAX_WORDS_COUNT
 
 
 class JSONSerializer(serializers.Serializer):
-    text = serializers.CharField(max_length=2000)
+    text = serializers.CharField(max_length=2000, label="")
+    arr = serializers.ListField(child=serializers.CharField(), allow_empty=False, min_length=1,
+                                max_length=MAX_WORDS_COUNT, label="")
 
-
-class TextModelSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Text
+    def validate(self, attrs):
+        """ В зависимости от исправности функционала на стороне UI(фронт-енде),
+        ожидается снять часть работы с бэк-енда, выполнив split строк на стороне UI.
+        Таким образом: одно из полей сериализатора должно быть заполнено."""
+        if not any([attrs["text"], attrs["arr"]]) or all([attrs["text"], attrs["arr"]]):
+            raise serializers.ValidationError
+        if attrs["arr"]:
+            re_ = re.compile(r"\b", flags=re.S)
+            for word in attrs["arr"]:
+                if re_.match(word).groups():
+                    raise serializers.ValidationError("С UI пришли недействительные данные!")
